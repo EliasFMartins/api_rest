7c07dea38f1714635fcc2988d5a3e141afd06bf8
Elias F Martins <eliasmartinsdev8@gmail.com>
2022-11-07 14:02:26 -0300

    feat

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -3,26 +3,28 @@ import multerConfig from '../config/multerConfig';
 import Foto from '../models/Foto';
 
 const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   // eslint-disable-next-line consistent-return
   store(req, res) {
-    try {
-      return upload(req, res, async (error) => {
-        if (error) {
-          return res.status(400).json({
-            errors: [error.code],
-          });
-        }
-
+    return upload(req, res, async (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+      try {
         const { originalname, filename } = req.file;
         const { aluno_id } = req.body;
         const foto = await Foto.create({ originalname, filename, aluno_id });
 
         return res.json(foto);
-      });
-    } catch (e) {
-      console.log(e);
-    }
+      } catch (e) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+    });
   }
 }
 export default new FotoController(); // para exportar instaciada e so colocar new e o parantes

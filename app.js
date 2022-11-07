e8c1b95cb5c114b689d37b5ee4480b9f157a612d
Elias F Martins <eliasmartinsdev8@gmail.com>
2022-11-07 14:28:47 -0300

    feat

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-
+import { dirname, resolve } from 'path';
 dotenv.config();
 
 import './src/database';
@@ -22,6 +22,7 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, 'uploads')));
   }
 
   routes() {

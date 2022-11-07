4b064ee444e957d04a06862762667e6275aa4bc5
Elias F Martins <eliasmartinsdev8@gmail.com>
2022-11-07 12:38:59 -0300

    feat

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,7 +1,20 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
 class FotoController {
+  // eslint-disable-next-line consistent-return
   async store(req, res) {
     try {
-      res.json(req.file);
+      return upload(req, res, (error) => {
+        if (error) {
+          return res.status(400).json({
+            errors: [error.code],
+          });
+        }
+
+        return res.json(req.file);
+      });
     } catch (e) {
       console.log(e);
     }
